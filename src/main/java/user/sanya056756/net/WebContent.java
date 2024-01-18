0508b25390ed65c0cacea5bca384b4046a979236
Sanya056756 <sashatop4ik56565@gmail.com>
2024-01-18 18:57:55 +0300

    Bug "stream is closed" fixed

--- src/main/java/user/sanya056756/net/WebContent.java
+++ src/main/java/user/sanya056756/net/WebContent.java
@@ -6,10 +6,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,21 +31,29 @@ import java.util.Map;
 
 public class WebContent {
 
-    public static InputStream getData(String url, Map<String, String> headers, Map<String, String> post)
-            throws IOException, WebContentException {
-        if (headers == null && post == null)
+    /**
+     * <li>Text or other data types</li>
+     */
+
+    public static InputStream getData(String url, Map<String, String> headers, Map<String, String> post) throws IOException {
+
+        boolean postNonNull = post != null;
+        boolean headersNonNull = headers != null;
+
+        if (!headersNonNull && !postNonNull)
             return getWebInputStream(url);
 
         HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
-        con.setRequestMethod(post != null ? "POST" : "GET");
+
+        con.setRequestMethod(postNonNull ? "POST" : "GET"); // Type POST or GET
 
         con.setRequestProperty("User-Agent", "WebContent/1.0"); // Default value
 
-        if (headers != null)
+        if (headersNonNull)
             for (Map.Entry<String, String> entry : headers.entrySet())
                 con.setRequestProperty(entry.getKey(), entry.getValue());
 
-        if (post != null) {
+        if (postNonNull) {
             con.setDoOutput(true);
             try (OutputStream os = con.getOutputStream()) {
                 os.write(getStringPost(post).getBytes(StandardCharsets.UTF_8));
@@ -58,45 +63,30 @@ public class WebContent {
         int responseCode = con.getResponseCode();
 
         if (responseCode != HttpURLConnection.HTTP_OK)
-            throw new WebContentException("The site showed an error: " + responseCode);
+            throw new AccessDeniedException("The site showed an error: " + responseCode);
 
-        try (InputStream is = con.getInputStream()){
-            return is;
-        } finally {
-            con.disconnect();
-        }
+        InputStream is = con.getInputStream();
+
+        con.disconnect();
+
+        return is;
     }
 
     public static InputStream getData(String url) throws IOException {
         return getWebInputStream(url);
     }
 
-    public static InputStream getDataHeader(String url, Map<String, String> headers)
-            throws WebContentException, IOException {
+    public static InputStream getDataHeader(String url, Map<String, String> headers) throws IOException {
         return getData(url, headers, null);
     }
 
-    public static InputStream getDataPost(String url, Map<String, String> post)
-            throws WebContentException, IOException {
+    public static InputStream getDataPost(String url, Map<String, String> post) throws IOException {
         return getData(url, null, post);
     }
 
     /**
      * <li>Private special methods</li>
      */
-
-    private static InputStream getDataType(String url, Map<String, String> headers, Map<String, String> post, String dataType)
-            throws WebContentException, IOException {
-        if (headers == null)
-            headers = new HashMap<>();
-
-        headers.put("Accept", "application/" + dataType);
-
-        if (post != null)
-            headers.put("Content-Type", "application/x-www-form-urlencoded");
-
-        return getData(url, headers, post);
-    }
 
     private static String getStringPost(Map<String, String> post) {
         StringBuilder builder = new StringBuilder();
@@ -105,8 +95,7 @@ public class WebContent {
 
         for (Map.Entry<String, String> entry : post.entrySet()) {
             builder.append(entry.getKey()).append("=").append(entry.getValue());
-            i++;
-            if (i == size)
+            if (i++ == size)
                 return builder.toString();
             builder.append("&");
         }
@@ -118,22 +107,19 @@ public class WebContent {
      * <li>JSON</li>
      */
 
-    public static InputStream getJsonData(String url, Map<String, String> headers, Map<String, String> post)
-            throws WebContentException, IOException {
+    public static InputStream getJsonData(String url, Map<String, String> headers, Map<String, String> post) throws IOException {
         return getDataType(url, headers, post, "json");
     }
 
-    public static InputStream getJsonData(String url) throws WebContentException, IOException {
+    public static InputStream getJsonData(String url) throws IOException {
         return getJsonData(url, null, null);
     }
 
-    public static InputStream getJsonDataHeader(String url, Map<String, String> headers)
-            throws WebContentException, IOException {
+    public static InputStream getJsonDataHeader(String url, Map<String, String> headers) throws IOException {
         return getJsonData(url, headers, null);
     }
 
-    public static InputStream getJsonDataPost(String url, Map<String, String> post)
-            throws WebContentException, IOException {
+    public static InputStream getJsonDataPost(String url, Map<String, String> post) throws IOException {
         return getJsonData(url, null, post);
     }
 
@@ -141,35 +127,49 @@ public class WebContent {
      * <li>XML</li>
      */
 
-    public static InputStream getXmlData(String url, Map<String, String> headers, Map<String, String> post)
-            throws WebContentException, IOException {
+    public static InputStream getXmlData(String url, Map<String, String> headers, Map<String, String> post) throws IOException {
         return getDataType(url, headers, post, "xml");
     }
 
-    public static InputStream getXmlData(String url) throws WebContentException, IOException {
+    public static InputStream getXmlData(String url) throws IOException {
         return getXmlData(url, null, null);
     }
 
-    public static InputStream getXmlDataHeader(String url, Map<String, String> headers)
-            throws WebContentException, IOException {
+    public static InputStream getXmlDataHeader(String url, Map<String, String> headers) throws IOException {
         return getXmlData(url, headers, null);
     }
 
-    public static InputStream getXmlDataPost(String url, Map<String, String> post)
-            throws WebContentException, IOException {
+    public static InputStream getXmlDataPost(String url, Map<String, String> post) throws IOException {
         return getXmlData(url, null, post);
     }
 
     /**
-     * <strong>
-     *     Without post and headers args.
-     *     <p>
-     *     Faster than getData(String, Map, Map).
-     * </strong>
+     * Without post and headers args.
+     * <p>
+     * Faster than getData(String, Map, Map).
      */
 
     public static InputStream getWebInputStream(String url) throws IOException {
         return new URL(url).openStream();
+    }
+
+    /**
+     * getData with data type
+     * @param dataType For example json or xml
+     */
+
+    public static InputStream getDataType(String url, Map<String, String> headers, Map<String, String> post, String dataType)
+            throws IOException {
+
+        if (headers == null)
+            headers = new HashMap<>();
+
+        headers.put("Accept", "application/" + dataType);
+
+        if (post != null)
+            headers.put("Content-Type", "application/x-www-form-urlencoded");
+
+        return getData(url, headers, post);
     }
 
     /**
@@ -201,17 +201,6 @@ public class WebContent {
             return true;
         } catch (Exception ignored) {
             return false;
-        }
-    }
-
-    /**
-     * <li>Exception <strong>WenContentException</strong></li>
-     */
-
-    public static class WebContentException extends Exception {
-
-        WebContentException(String message) {
-            super(message);
         }
     }
 }
